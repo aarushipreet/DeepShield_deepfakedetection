31e9de3b6fc449742de2e94cbb43443102ed7033
aarushipreet <aarushipreetbrar@gmail.com>
2026-03-14 23:46:46 +0530

    Create preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,18 @@
+import cv2
+import os
+
+def extract_frames(video_path, output_folder):
+    cap = cv2.VideoCapture(video_path)
+    count = 0
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        frame_path = os.path.join(output_folder, f"frame_{count}.jpg")
+        cv2.imwrite(frame_path, frame)
+        count += 1
+
+    cap.release()
+    print(f"{count} frames extracted.")
